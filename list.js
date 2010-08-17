a9394de4baa92c3ecbe135eb43d438a7b5bca6b6
Bert Goethals <bert@bertg.be>
2010-08-17 18:37:49 +0200

    Some nice rewrite of the plugin. * Allow multiple instances per page * Start from a multiple select * load options via Javascript * no dependance on images

---
+++ list.js
@@ -0,0 +1,30 @@
+[
+  {
+    "value": 0,
+    "name": "Bert"
+  },
+  {
+    "value": 1,
+    "name": "Johan"
+  },
+  {
+    "value": 2,
+    "name": "Andrew"
+  },
+  {
+    "value": 3,
+    "name": "Amaury"
+  },
+  {
+    "value": 4,
+    "name": "Edouard"
+  },
+  {
+    "value": 5,
+    "name": "Antoine"
+  },
+  {
+    "value": 6,
+    "name": "Thierry"
+  }
+]
