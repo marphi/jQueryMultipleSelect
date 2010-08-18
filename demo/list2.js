62c8e9bf7cfa2144861ecd72ffdd315252221fec
Bert Goethals <bert@bertg.be>
2010-08-18 16:00:02 +0200

    Allowing space to be added for extra content. Also added example

---
+++ demo/list2.js
@@ -0,0 +1,37 @@
+[
+  {
+    "value": 0,
+    "name": "Bert",
+    "groups": ["1", "2"]
+  },
+  {
+    "value": 1,
+    "name": "Johan",
+    "groups": ["3", "2"]
+  },
+  {
+    "value": 2,
+    "name": "Andrew",
+    "groups": ["2"]
+  },
+  {
+    "value": 3,
+    "name": "Amaury",
+    "groups": ["1", "2"]
+  },
+  {
+    "value": 4,
+    "name": "Edouard",
+    "groups": ["4"]
+  },
+  {
+    "value": 5,
+    "name": "Antoine",
+    "groups": ["3"]
+  },
+  {
+    "value": 6,
+    "name": "Thierry",
+    "groups": ["1", "4"]
+  }
+]
